f978a0186fcd4e2f50381188663af9dacc83db9f
chenyuran <19302010083@fudan.edu.cn>
2025-03-30 12:50:31 +0800

    恢复inflow

--- Mrlaw-portal/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Chat } from 'inflow/components/chat';
-import { getModels } from 'inflow/lib/config/models';
-import { generateId } from 'ai'
-
-export default async function HomePage() {
-    const id = generateId()
-    const models = await getModels()
-    return <Chat id={id} models={models} />
-}
